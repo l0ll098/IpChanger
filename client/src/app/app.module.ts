24069d7dd956e8f4f9871b7899013d43ca897782
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-02-11 18:29:07 +0100

    Implemented a basic app shell

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material";
+
+import { environment } from "../environments/environment";
 
 import { AppComponent } from "./app.component";
+import { AppShellComponent } from "./shell/app-shell.component";
+
+
+const appRoutes: Routes = [
+	{
+		path: "",
+		component: AppShellComponent,
+		children: [
+
+		]
+	}
+];
+
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		AppShellComponent
 	],
 	imports: [
 		BrowserModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		RouterModule,
+		RouterModule.forRoot(
+			appRoutes,
+			{ enableTracing: (environment.enableAngularRoutingLog ? true : false) }
+		),
+		HttpClientModule,
+		// Angular Material
+		MatToolbarModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ client/src/app/shell/app-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+	selector: "app-shell",
+	templateUrl: "app-shell.component.html",
+	styleUrls: ["app-shell.component.css"],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppShellComponent {
+
+}
