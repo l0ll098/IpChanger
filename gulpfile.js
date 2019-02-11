04af0bd6f762743a8fc759bd0e151a497f8e0a35
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-02-11 15:08:23 +0100

    Added gulp. Not fully tested since the client side is missing

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const exec = require("gulp-exec");
+const del = require("del");
+const serverProject = ts.createProject("tsconfig.json");
+
+const DIST_FOLDER = "./dist/";
+
+
+const Tasks = Object.freeze({
+    BuildServer: "BuildServer",
+    CopyPackageJson: "CopyPackageJson",
+    CopyPackageLockJson: "CopyPackageLockJson",
+
+    BuildClient: "BuildClient",
+    CopyClientDist: "CopyClientDist",
+
+    BuildAndCopyFiles: "BuildAndCopyFiles",
+    Clean: "Clean"
+});
+
+
+/**
+ * Compile server code
+ */
+gulp.task(Tasks.BuildServer, () => {
+    return serverProject
+        .src()
+        .pipe(serverProject())
+        .js.pipe(gulp.dest(DIST_FOLDER));
+});
+
+/**
+ * Copy package.json and package-lock.json to the dist folder
+ */
+gulp.task(Tasks.CopyPackageJson, () => {
+    return gulp
+        .src("./package.json")
+        .pipe(gulp.dest(DIST_FOLDER));
+});
+
+gulp.task(Tasks.CopyPackageLockJson, () => {
+    return gulp.src("./package-lock.json")
+        .pipe(gulp.dest(DIST_FOLDER));
+});
+
+/**
+ * Builds the client side (using Angular CLI)
+ */
+gulp.task(Tasks.BuildClient, () => {
+    return gulp
+        .src("./client/")
+        .pipe(exec("npm run build", {
+            cwd: "./client/"
+        }));
+});
+
+/**
+ * Copies the compiled client dist folder in the server one
+ */
+gulp.task(Tasks.CopyClientDist, () => {
+    return gulp.src("client/dist/**/*")
+        .pipe(gulp.dest(DIST_FOLDER + "client/"));
+});
+
+gulp.task(Tasks.Clean, () => {
+    return del([
+        "dist/**",
+        "bin/**",
+        "client/dist/**"
+    ]);
+});
+
+/**
+ * Run all
+ */
+gulp.task(Tasks.BuildAndCopyFiles, (done) => {
+    return gulp.series(
+        Tasks.Clean,
+        Tasks.BuildServer,
+        Tasks.BuildClient,
+        Tasks.CopyClientDist,
+        gulp.parallel(
+            Tasks.CopyPackageJson,
+            Tasks.CopyPackageLockJson
+        )
+    )(done);
+});
+
+gulp.task("default", (done) => {
+    return gulp.series(
+        Tasks.BuildAndCopyFiles,
+        // Tasks.PackageAll
+    )(done);
+});
