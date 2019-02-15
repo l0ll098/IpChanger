1ab73f627ef3eacef672c3fec208fbd889369b74
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-02-15 11:56:57 +0100

    Implement the form with toggle buttons

--- client/src/app/components/addresses-list/addresses-list.component.ts
+++ client/src/app/components/addresses-list/addresses-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material";
+import { FormGroup, FormControl } from "@angular/forms";
+import { MatDialog, MatSlideToggleChange } from "@angular/material";
 
 import { HttpService } from "../../services/http.service";
 import { Address } from "../../models/Address";
@@ -15,15 +16,24 @@ import { DialogComponent } from "../dialog/dialog.component";
 export class AppAddressesListComponent implements OnInit {
 
 	public addresses: Address[] = [];
+	public addressFG: FormGroup = new FormGroup({});
 
 	constructor(
 		private httpService: HttpService,
 		private dialog: MatDialog) { }
 
 	ngOnInit(): void {
+		let id = 0;
+
 		this.getAddresses()
 			.then((addr) => {
 				this.addresses = addr;
+
+
+				// Initialize the FormGroup
+				this.addresses.forEach(() => {
+					this.addressFG.addControl("fc" + (id++), new FormControl(false));
+				});
 			})
 			.catch((err) => {
 				console.log(err);
@@ -45,6 +55,48 @@ export class AppAddressesListComponent implements OnInit {
 			return Promise.resolve(await this.httpService.getAddresses());
 		} catch (err) {
 			return Promise.reject(err);
+		}
+	}
+
+	public onchange(event: MatSlideToggleChange, id: number) {
+		let alreadyChecked = false;
+		for (let i = 0; i < this.addresses.length; i++) {
+			// If the id is equal to the index, the corresponding form control has been already changed
+			if (i === id) {
+				if (!this.addressFG.controls["fc" + i].value) {
+					alreadyChecked = true;
+					break;
+				}
+			} else {
+				if (this.addressFG.controls["fc" + i].value) {
+					alreadyChecked = true;
+					break;
+				}
+			}
+		}
+
+		// If there aren't other slide toggles have been already toggled, proceed changing ip
+		if (!alreadyChecked) {
+
+			if (event.checked) {
+				console.log("call run change ip on val: ", this.addresses[id]);
+			}
+
+		} else {
+			// Otherwise set it back to it's previous value
+			if (this.addressFG.controls["fc" + id].value) {
+				this.addressFG.controls["fc" + id].setValue(!this.addressFG.controls["fc" + id].value);
+
+				this.dialog.open(DialogComponent, {
+					data: {
+						title: "Error",
+						message: "Only one can be checked. Deselect the other and retry.",
+						doActionBtn: {
+							text: "Ok"
+						}
+					}
+				});
+			}
 		}
 	}
 }
